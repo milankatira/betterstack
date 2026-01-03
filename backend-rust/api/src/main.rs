a8f92df4bcd8048fc71214048a9d56232bff4f99
milankatira <milankatira26@gmail.com>
2026-01-03 13:18:10 +0530

    feat: update Cargo.toml and main.rs to include poem dependency and implement hello handler

--- backend-rust/api/src/main.rs
+++ backend-rust/api/src/main.rs
@@ -1,6 +1,14 @@
-use std::fmt::Error;
+use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
+
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    format!("hello: {}", name)
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    print!("Hello, world!");
-    Ok(())
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new().at("/hello/:name", get(hello));
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+      .run(app)
+      .await
 }
